d4ad2414234d02a456b5d229ff851576e8ad82a2
Nathan Williams <nate88williams@gmail.com>
2022-04-22 16:58:45 -0500

    Completed the mutation fuzzer and updated the protocol fuzzer. Added output files for both to load into lexer.py.

--- mutation_fuzzer.py
+++ mutation_fuzzer.py
@@ -1,16 +1,13 @@
 import random
-
-select_keywords = ["then", "else", "end"]
-loop_keywords = ["while", "do", "for", "end"]
-operators = ["+", "-", "*", "/", "%", "not", "("]
-logical_operators = ["<", ">", "<=", ">=", "==", "!=", "and", "or"]
-io_keywords = ["print", "get"]
+import re
 
 value = ["(", "not", "-"]
 v_expr = [">", ">=", "<", "<=", "==", "!="]
 f_expr = ["*", "/", "%"]
 t_expr = ["+", "-"]
 b_expr = ["and", "or"]
+
+
 
 
 def gen_code():
@@ -31,7 +28,17 @@ def gen_code():
                 sub_choice = random.randint(1, 3)
                 fuzz = std_cmd(sub_choice, fuzz) + "\n"
             fuzz = fuzz + "end;\n"
-
+        else:
+            while_length = random.randint(1, 3)
+            fuzz = fuzz + "while " + get_expr() + " do\n"
+            for y in range(0, while_length):
+                sub_choice = random.randint(1, 3)
+                fuzz = std_cmd(sub_choice, fuzz) + "\n"
+            fuzz = fuzz + "end;\n"
+    # fuzz = '\n'.join(i.strip() for i in fuzz.split('\n'))
+    # fuzz = ' '.join(i.strip() for i in fuzz.split(' '))
+    # fuzz = re.sub("\n+", "\n", fuzz)
+    fuzz = re.sub("\s+", " ", fuzz)
     return fuzz
 
 
@@ -105,7 +112,7 @@ def get_value(term):
     else:
         choice = random.randint(0, 4)
         if choice == 0:
-            return value[choice] + get_expr() + ")"
+            return value[choice] + " " + get_expr() + ")"
         elif choice == 3:
             return get_id()
         elif choice == 4:
@@ -159,4 +166,25 @@ def get_comment():
         return ""
 
 
-print(gen_code())
+def mutate(mut_string):
+    mut_string = mut_string.replace("print", get_id())
+    mut_string = mut_string.replace("get", get_id())
+    mut_string = mut_string.replace("if", get_id())
+    mut_string = mut_string.replace("then", get_id())
+    mut_string = mut_string.replace("else", get_id())
+    mut_string = mut_string.replace("end", get_id())
+    mut_string = mut_string.replace("while", get_id())
+    mut_string = mut_string.replace("do", get_id())
+    mut_string = mut_string.replace("end", get_id())
+    mut_string = mut_string.replace("and", get_id())
+    mut_string = mut_string.replace("or", get_id())
+    mut_string = mut_string.replace("not", get_id())
+    mut_string = mut_string.replace("for", get_id())
+    return mut_string
+
+
+if __name__ == "__main__":
+    string = gen_code()
+    string = mutate(string)
+    f = open("mutated_code.txt", "w")
+    f.write(string)
--- protocol_fuzzer.py
+++ protocol_fuzzer.py
@@ -1,68 +1,122 @@
 import random
+import re
 
-select_keywords = ["then", "else", "end"]
-loop_keywords = ["while", "do", "for", "end"]
-operators = ["+", "-", "*", "/", "%", "not", "("]
-logical_operators = ["<", ">", "<=", ">=", "==", "!=", "and", "or"]
-io_keywords = ["print", "get"]
-# Remaining operators: "=", ";", ")", "if"
-stmt = ["print", "input", "assign", "if", "while", "for"]
-
-if_while_count = 0
+value = ["(", "not", "-"]
+v_expr = [">", ">=", "<", "<=", "==", "!="]
+f_expr = ["*", "/", "%"]
+t_expr = ["+", "-"]
+b_expr = ["and", "or"]
 
 
-def if_while_limit_reached():
-    global if_while_count
-    # print(if_while_count)
-    if if_while_count > 3:
-        return True
-    if_while_count = if_while_count + 1
-    return False
+def gen_code():
+    fuzz = ""
+    length = random.randint(1, 5)
+    for i in range(0, length):
+        choice = random.randint(1, 5)
+        if choice <= 3:
+            fuzz = std_cmd(choice, fuzz)
+        elif choice == 4:
+            if_length = random.randint(1, 3)
+            fuzz = fuzz + "if " + get_expr() + " then\n"
+            for y in range(0, if_length):
+                sub_choice = random.randint(1, 3)
+                fuzz = std_cmd(sub_choice, fuzz) + "\n"
+            fuzz = fuzz + "else\n"
+            for y in range(0, if_length):
+                sub_choice = random.randint(1, 3)
+                fuzz = std_cmd(sub_choice, fuzz) + "\n"
+            fuzz = fuzz + "end;\n"
+        else:
+            while_length = random.randint(1, 3)
+            fuzz = fuzz + "while " + get_expr() + " do\n"
+            for y in range(0, while_length):
+                sub_choice = random.randint(1, 3)
+                fuzz = std_cmd(sub_choice, fuzz) + "\n"
+            fuzz = fuzz + "end;\n"
+    # fuzz = '\n'.join(i.strip() for i in fuzz.split('\n'))
+    # fuzz = ' '.join(i.strip() for i in fuzz.split(' '))
+    # fuzz = re.sub("\n+", "\n", fuzz)
+    fuzz = re.sub("\s+", " ", fuzz)
+    return fuzz
 
 
+def std_cmd(choice, fuzz):
+    if choice == 1:
+        fuzz = fuzz + "print "
+        sub_choice = random.randint(1, 2)
+        if sub_choice == 1:
+            fuzz = fuzz + get_str() + ";\n"
+        else:
+            fuzz = fuzz + get_expr() + ";\n"
+    elif choice == 2:
+        fuzz = fuzz + "get " + get_id() + ";\n"
+    elif choice == 3:
+        fuzz = fuzz + get_id() + " = " + get_expr() + ";\n"
+    return fuzz
+
+
+# Expressions
 def get_expr():
-    return "test"
+    return get_n_expr() + " " + get_b_expr()
 
 
-def get_stmt():
-    output = random.choice([get_print(), get_input(), get_assign(), get_if(), get_while()])
-    return output
-
-
-def get_stmt_list(count):
-    if count > 2:
-        return "\n"
-    count = count + 1
-    output = get_stmt()
-    return output + ";\n" + get_stmt_list(count)  # random.choice(["", get_stmt_list(count)])
-
-
-# Functions to return statements
-
-
-def get_while():
-    # if if_while_limit_reached():
-    #     return ""
-    # return "while " + get_expr() + " do\n" + get_stmt_list(0) + "\nend"
-    return "while " + get_expr() + "do" + get_stmt_list(1)
-
-
-def get_if():
-    if if_while_limit_reached():
+def get_b_expr():
+    choice = random.randint(0, 8)
+    if choice > 1:
         return ""
-    return "if " + get_expr() + " then\n" + get_stmt_list(0) + "else " + get_stmt_list(0) + "\nend"
+    return b_expr[choice] + " " + get_n_expr()
 
 
-def get_assign():
-    return get_id() + " = " + get_expr()
+def get_n_expr():
+    return get_term() + " " + get_t_expr()
 
 
-def get_input():
-    return "input " + get_id()
+def get_t_expr():
+    choice = random.randint(0, 8)
+    if choice > 1:
+        return ""
+    return t_expr[choice] + " " + get_n_expr()
 
 
-def get_print():
-    return "print " + random.choice([get_str(), get_expr()])
+def get_term():
+    return get_factor()
+
+
+def get_f_expr():
+    choice = random.randint(0, 6)
+    if choice > 2:
+        return ""
+    return f_expr[choice] + " " + get_term()
+
+
+def get_factor():
+    return get_value(False) + " " + get_v_expr()
+
+
+def get_v_expr():
+    choice = random.randint(0, 10)
+    if choice > 5:
+        return "" + get_f_expr()  # Call f_expr because there is no logical expression
+    return v_expr[choice] + " " + get_value(False)
+
+
+def get_value(term):
+    if term:
+        choice = random.randint(0, 1)
+        if choice == 0:
+            return get_id()
+        else:
+            return get_digit()
+    else:
+        choice = random.randint(0, 4)
+        if choice == 0:
+            return value[choice] + get_expr() + ")"
+        elif choice == 3:
+            return get_id()
+        elif choice == 4:
+            return get_digit()
+        else:
+            return value[choice] + get_value(True)
 
 
 # Functions to return strings, id's, digits and comments
@@ -87,10 +141,8 @@ def get_id():
 
 def get_digit():
     var = ""
-    sign = ["", "+", "-"]
     num_char = "0123456789"
     length = random.randint(1, 10)
-    var = sign[random.randint(0, 2)]
     for i in range(0, length):
         var = var + num_char[random.randint(0, 9)]
     return var
@@ -112,12 +164,7 @@ def get_comment():
         return ""
 
 
-# Driver
-def prot_fuzzer():
-    return get_stmt_list(0)
-
-    # Break up the keywords into categories and make an if for each one. Structure each one appropriately.
-
-
-# if __name__ == "__main__":
-print(prot_fuzzer())
+if __name__ == "__main__":
+    string = gen_code()
+    f = open("protocol_fuzz.txt", "w")
+    f.write(string)
