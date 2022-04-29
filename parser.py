e17bc161e36cc5958da737b9034d58cdbf8d5e4d
Nathan Williams <nate88williams@gmail.com>
2022-04-29 11:54:35 -0500

    Updated parser.py code. Added functions up to parse_expr().

--- parser.py
+++ parser.py
@@ -17,6 +17,7 @@ def lex():
 def parse_prog():
     return parse_stmt_list()
 
+
 def parse_stmt_list():
     result = parse_stmt()
     if result:
@@ -27,31 +28,59 @@ def parse_stmt_list():
 
 def parse_stmt():
     global nextToken
-    if nextToken == "print":
+    if nextToken[1] == "print":
         lex()
         if nextToken[0] == lexer.STRING:
             return True
         else:
             return parse_expr()
-    elif nextToken == "input":
-        pass
-    elif nextToken == "assign":
-        pass
-    elif nextToken == "assign":
-        pass
-    elif nextToken == "if":
-        pass
-    elif nextToken == "while":
-        pass
-    elif nextToken == "for":
-        pass
-    else:
+    elif nextToken[1] == "get":
+        lex()
+        if nextToken[0] == lexer.ID:
+            return True
+    elif nextToken[0] == lexer.ID:
+        lex()
+        if nextToken[0] == lexer.ASSIGN:
+            lex()
+            return parse_expr()
+    elif nextToken[1] == "if":
+        lex()
+        if parse_expr():
+            lex()
+            if nextToken[1] == "then":
+                lex()
+                if parse_stmt_list():
+                    lex()
+                    if nextToken[1] == "else":
+                        lex()
+                        if parse_stmt_list():
+                            lex()
+                            if nextToken[1] == "end":
+                                return True
+    elif nextToken[1] == "while":
+        lex()
+        if parse_expr():
+            lex()
+            if nextToken[1] == "do":
+                lex()
+                if parse_stmt_list():
+                    lex()
+                    if nextToken[1] == "end":
+                        return True
+    elif nextToken[1] == "for":    # Finish for loop
+        print("for loop")
+    elif nextToken[0] == lexer.INPUT_ERROR:  # Input error found.
         return False
+    return False    # Parser cannot parse input. Return False.
 
 
 def parse_expr():
-    pass
+    return
+
 
 lex_input = list(sys.stdin.read())
 lex()
-parse_prog()
+if parse_prog():
+    print("This program is correct.")
+else:
+    print("This program is not correct.")
